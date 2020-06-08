1da5d9a978845708156d6d00c06681f8a9d8e904
Casandra Carrizales <cc@MacBook-Pro.local>
2020-06-08 03:55:47 -0500

    commit hw
    Signed-off-by: Casandra Carrizales <cc@MacBook-Pro.local>

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-    API_KEY = "sk.eyJ1IjoiY2MyMjIyMjIyMiIsImEiOiJja2I0anRtZ2kwdW43MnltdnR0bzh0Yng5In0.562swyLngv1_U1jnDbfz4Q";
+API_KEY = "sk.eyJ1IjoiY2MyMjIyMjIyMiIsImEiOiJja2I0anRtZ2kwdW43MnltdnR0bzh0Yng5In0.562swyLngv1_U1jnDbfz4Q";
